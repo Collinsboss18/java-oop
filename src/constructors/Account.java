927c2714fec1d79592b82e000d642193e7682852
Abadaike Collins <abadaikecollins@gmail.com>
2021-01-21 17:03:57 +0100

    created deposit and withdrawal method

--- src/constructors/Account.java
+++ src/constructors/Account.java
@@ -4,6 +4,20 @@ public class Account {
 	private String number, costomerName, costomerEmail, costomerPhone;
 	private double balance;
 	
+	public void deposite(double depositAmount) {
+		this.balance += depositAmount;
+		System.out.println("Deposit of " + depositAmount + " made. new balance is " + this.balance);
+	}
+	
+	public void withdrawal(double withdrawalAmount) {
+		if (this.balance - withdrawalAmount <= 0) {
+			System.out.println("Only " + this.balance + " available. Withdrawal not processed.");
+		} else {
+			this.balance -= withdrawalAmount;
+			System.out.println("Withdrawal of " + withdrawalAmount + " proccessed. Remaining balance = " + this.balance);;
+		}
+	}
+	
 	public String getNumber() {
 		return number;
 	}
