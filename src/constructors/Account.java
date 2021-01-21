3616435830ff002753d46493798ee9f3a53f3448
Abadaike Collins <abadaikecollins@gmail.com>
2021-01-21 17:03:17 +0100

    updated account class with getters and starters

--- src/constructors/Account.java
+++ src/constructors/Account.java
@@ -1,5 +1,42 @@
 package constructors;
 
 public class Account {
-
+	private String number, costomerName, costomerEmail, costomerPhone;
+	private double balance;
+	
+	public String getNumber() {
+		return number;
+	}
+	public void setNumber(String number) {
+		this.number = number;
+	}
+	
+	public String getCostomerName() {
+		return costomerName;
+	}
+	public void setCostomerName(String costomerName) {
+		this.costomerName = costomerName;
+	}
+	
+	public String getCostomerEmail() {
+		return costomerEmail;
+	}
+	public void setCostomerEmail(String costomerEmail) {
+		this.costomerEmail = costomerEmail;
+	}
+	
+	public String getCostomerPhone() {
+		return costomerPhone;
+	}
+	public void setCostomerPhone(String costomerPhone) {
+		this.costomerPhone = costomerPhone;
+	}
+	
+	public double getBalance() {
+		return balance;
+	}
+	public void setBalance(double balance) {
+		this.balance = balance;
+	}
+	
 }
