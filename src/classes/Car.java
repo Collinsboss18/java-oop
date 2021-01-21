f0a1312c9a8a3b8ebeb9a327af53bfac40c9044f
Abadaike Collins <abadaikecollins@gmail.com>
2021-01-21 15:56:07 +0100

    created Main and Car class

---
+++ src/classes/Car.java
@@ -0,0 +1,17 @@
+package classes;
+
+/**
+ * @since  Jan 12 15:14 2021
+ * @author Collins Abadaike
+ */
+
+public class Car {
+	
+	private int doors, wheels;
+	private String model, engnine, color;
+	
+	public void setModel(String model) {
+		this.model = model;
+	}
+
+}
---
+++ src/classes/Main.java
@@ -0,0 +1,15 @@
+package classes;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// 
+		
+		Car porsche = new Car();
+		Car holden = new Car();
+		
+		porsche.setModel("Carrera");
+
+	}
+
+}
