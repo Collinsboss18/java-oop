1cf334df3f457d47318a4f923faae239f2ba7a64
Abadaike Collins <abadaikecollins@gmail.com>
2021-01-21 17:15:08 +0100

    testing account class method

--- src/constructors/Main.java
+++ src/constructors/Main.java
@@ -4,6 +4,16 @@ public class Main {
 
 	public static void main(String[] args) {
 		// 
+		Account bobsAccount = new Account();
+		bobsAccount.setNumber("183127232");
+		bobsAccount.setBalance(0.00);
+		bobsAccount.setCostomerName("Bob Brown");
+		bobsAccount.setCostomerPhone("90433");
+		bobsAccount.setCostomerEmail("bob@gmail.com");
+		
+		bobsAccount.withdrawal(100);
+		bobsAccount.deposite(100);
+		bobsAccount.withdrawal(50);
 
 	}
 
