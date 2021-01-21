c5d40c31ad7af00e9bfe69683403dab6e2e36fe3
Abadaike Collins <abadaikecollins@gmail.com>
2021-01-21 15:44:54 +0100

    updated rectangle and trangle class

--- src/inheritance/Rectangle.java
+++ src/inheritance/Rectangle.java
@@ -5,4 +5,5 @@ public class Rectangle extends Polygon {
 	public double area () {
 		return (height*width);
 	}
+	
 }
--- src/inheritance/Triangle.java
+++ src/inheritance/Triangle.java
@@ -5,4 +5,5 @@ public class Triangle extends Polygon {
 	public double area () {
 		return (height*width)/2;
 	}
+	
 }
