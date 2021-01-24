214dab451c206ea46c836cc933f68af37e8f16e3
Abadaike Collins <abadaikecollins@gmail.com>
2021-01-24 02:28:03 +0100

    added accelerate method

--- src/inheritance2/Highlander.java
+++ src/inheritance2/Highlander.java
@@ -4,10 +4,30 @@ public class Highlander extends Car {
 	private int roadServiceMonths;
 
 	public Highlander(int roadServiceMonths) {
-		super("Highlander", "3M", 4, 4, 3, true);
+		super("Highlander", "3M", 4, 4, 4, true);
 		this.roadServiceMonths = roadServiceMonths;
 	}
 
+	public void accelerate (int rate) {
+		int newVelocity = getCurrentVelocity() + rate;
+		
+		if (newVelocity == 0) {
+			stop();
+			changeGear(1);
+		} else if (newVelocity > 0 && newVelocity <= 10) {
+			changeGear(1);
+		} else if (newVelocity > 10 && newVelocity <= 20) {
+			changeGear(2);
+		} else if (newVelocity > 20 && newVelocity <= 30) {
+			changeGear(3);
+		} else {
+			changeGear(4);
+		}
+		
+		if (newVelocity > 0) {
+			changeVelocity(newVelocity, getCurrentDirection());
+		}
+	}
 	
 
 }
