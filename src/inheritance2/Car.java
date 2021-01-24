7e6a4959f0d5973a19453ce59c18496be907ee13
Abadaike Collins <abadaikecollins@gmail.com>
2021-01-24 02:05:31 +0100

    Added constructor to car class

--- src/inheritance2/Car.java
+++ src/inheritance2/Car.java
@@ -1,5 +1,16 @@
 package inheritance2;
 
-public class Car {
-
+public class Car extends Vehicle {
+	private int wheels, door, gears, currentGear;
+	private boolean isManual;
+	public Car(String name, String size, int wheels, int door, int gears, boolean isManual) {
+		super(name, size);
+		this.wheels = wheels;
+		this.door = door;
+		this.gears = gears;
+		this.isManual = isManual;
+		this.currentGear = 1;
+	}
+	
+	
 }
