ab60a4128fa3a835c4bd188d1b7de83003144005
Abadaike Collins <abadaikecollins@gmail.com>
2021-01-24 02:00:24 +0100

    updated vehicle class with constructors and getters

--- src/inheritance2/Vehicle.java
+++ src/inheritance2/Vehicle.java
@@ -1,5 +1,42 @@
 package inheritance2;
 
 public class Vehicle {
+	private String name, size;
+	private int currentDirection, currentVelocity;
+	
+	public Vehicle(String name, String size) {
+		this.name = name;
+		this.size = size;
+		
+		this.currentVelocity = 0;
+		this.currentDirection = 0;
+	}
+	
+	public void steer(int direction) {
+		this.currentDirection += direction;
+		System.out.println("Vehicle steering at " + currentDirection + " degree");
+	}
+	
+	public void move(int velocity, int direction) {
+		this.currentVelocity = velocity;
+		this.currentDirection = direction;
+		System.out.println("Vehicle moving at " + currentVelocity + " in direction " + currentDirection);
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public String getSize() {
+		return size;
+	}
+
+	public int getCurrentDirection() {
+		return currentDirection;
+	}
+
+	public int getCurrentVelocity() {
+		return currentVelocity;
+	}
 
 }
