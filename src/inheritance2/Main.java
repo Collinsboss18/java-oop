4de66cc18b56eba4229d187a4aa78403cb59b594
Abadaike Collins <abadaikecollins@gmail.com>
2021-01-24 02:37:48 +0100

    updated class

--- src/inheritance2/Main.java
+++ src/inheritance2/Main.java
@@ -11,6 +11,7 @@ public class Main {
 	public static void main(String[] args) {
 		
 		Highlander highlander = new Highlander(21);
+		highlander.steer(45);
 		highlander.accelerate(20);
 		highlander.accelerate(14);
 
