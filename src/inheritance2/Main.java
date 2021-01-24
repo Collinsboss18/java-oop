4c911b9df3efff00d119cf45cccb2e8ad52269f9
Abadaike Collins <abadaikecollins@gmail.com>
2021-01-24 02:33:07 +0100

    testing classes

--- src/inheritance2/Main.java
+++ src/inheritance2/Main.java
@@ -1,9 +1,17 @@
 package inheritance2;
 
+/**
+ * @author Collins Abadaike
+ * 
+ * Class Project
+ * ************************
+ */
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		Highlander highlander = new Highlander(21);
+		highlander.accelerate(20);
 
 	}
 
