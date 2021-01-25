bc99f87f6288d1df9d9343e95dc2e710f15c3f91
Abadaike Collins <abadaikecollins@gmail.com>
2021-01-25 10:06:35 +0100

    created dimension class

---
+++ src/composition/Dimensions.java
@@ -0,0 +1,32 @@
+/**
+ * 
+ */
+package composition;
+
+/**
+ * @author Collins Abadaike
+ *
+ */
+public class Dimensions {
+	private int width, height, depth;
+
+	public Dimensions(int width, int height, int depth) {
+		super();
+		this.width = width;
+		this.height = height;
+		this.depth = depth;
+	}
+
+	public int getWidth() {
+		return width;
+	}
+
+	public int getHeight() {
+		return height;
+	}
+
+	public int getDepth() {
+		return depth;
+	}
+	
+}
