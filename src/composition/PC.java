082db257bdf27327cb2d7d31a48c3738b98ef61a
Abadaike Collins <abadaikecollins@gmail.com>
2021-01-25 10:34:15 +0100

    updated pc class

--- src/composition/PC.java
+++ src/composition/PC.java
@@ -9,5 +9,27 @@ package composition;
  */
 public class PC {
 	private Case theCase;
-
+	private Monitor monitor;
+	private MotherBoard motherBoard;
+	public PC(Case theCase, Monitor monitor, MotherBoard motherBoard) {
+		super();
+		this.theCase = theCase;
+		this.monitor = monitor;
+		this.motherBoard = motherBoard;
+	}
+	
+	public Case getTheCase() {
+		return theCase;
+	}
+	
+	public Monitor getMonitor() {
+		return monitor;
+	}
+	
+	public MotherBoard getMotherBoard() {
+		return motherBoard;
+	}
+	
+	
+	
 }
