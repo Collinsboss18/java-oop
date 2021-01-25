77358b9cb19ffc83d958f6283073530aecbf6877
Abadaike Collins <abadaikecollins@gmail.com>
2021-01-25 10:39:18 +0100

    updated main class

--- src/composition/Main.java
+++ src/composition/Main.java
@@ -13,6 +13,8 @@ public class Main {
 
 	public static void main(String[] args) {
 		// 
+		Dimensions dimensions = new Dimensions(20, 20, 5);
+		Case theCase = new Case("021", "Dell", "24000", dimensions)
 
 	}
 
