a091598bbc0a4faf0902ff8db28ca98fa953ea8a
Abadaike Collins <abadaikecollins@gmail.com>
2021-01-25 09:49:21 +0100

    created monitor class

---
+++ src/composition/Monitor.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package composition;
+
+/**
+ * @author Collins Abadaike
+ *
+ */
+public class Monitor {
+	private String model, manufactural;
+	private int size;
+	private Resolution nativeResolution;
+	
+	public Monitor(String model, String manufactural, int size, Resolution nativeResolution) {
+		super();
+		this.model = model;
+		this.manufactural = manufactural;
+		this.size = size;
+		this.nativeResolution = nativeResolution;
+	}
+	
+	public void drawPixelAt(int x, int y, String color) {
+		System.out.println("Drawing pixel at " + x + ", " + y + " in colour " + color);
+	}
+
+}
