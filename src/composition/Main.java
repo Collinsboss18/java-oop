bbfe407ddcb88637f587f994eadf738fb813286d
Abadaike Collins <abadaikecollins@gmail.com>
2021-01-25 11:48:25 +0100

    instanciated dimension, case monitor, motherboard class and created a PC

--- src/composition/Main.java
+++ src/composition/Main.java
@@ -14,7 +14,14 @@ public class Main {
 	public static void main(String[] args) {
 		// 
 		Dimensions dimensions = new Dimensions(20, 20, 5);
-		Case theCase = new Case("021", "Dell", "24000", dimensions)
+		Case theCase = new Case("021", "Dell", "24000", dimensions);
+		
+		
+		Monitor theMonitor = new Monitor("25inch beast", "Asa", 30, new Resolution(2540, 1440));
+		
+		MotherBoard theMotherBoard = new MotherBoard("BJ-200", "Asus", "v2.34", 2, 4);
+		
+		PC thePC = new PC(theCase, theMonitor, theMotherBoard);
 
 	}
 
