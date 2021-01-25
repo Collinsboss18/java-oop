62db750aea4c1bb980ea7907b319ae0e2887dbd1
Abadaike Collins <abadaikecollins@gmail.com>
2021-01-25 11:52:30 +0100

    testing methods

--- src/composition/Main.java
+++ src/composition/Main.java
@@ -10,9 +10,12 @@ package composition;
  * ************************
  */
 public class Main {
+	/**
+	 * Composition is best used over inheritance when you want to have access to more than one class
+	 */
 
 	public static void main(String[] args) {
-		// 
+		
 		Dimensions dimensions = new Dimensions(20, 20, 5);
 		Case theCase = new Case("021", "Dell", "24000", dimensions);
 		
@@ -22,6 +25,9 @@ public class Main {
 		MotherBoard theMotherBoard = new MotherBoard("BJ-200", "Asus", "v2.34", 2, 4);
 		
 		PC thePC = new PC(theCase, theMonitor, theMotherBoard);
+		thePC.getMonitor().drawPixelAt(12, 43, "pink");
+		thePC.getMotherBoard().loadProgram("File Manager");
+		thePC.getTheCase().pressPowerButton();
 
 	}
 
